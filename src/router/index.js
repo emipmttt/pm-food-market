36a4fe8c782a744f5141f2bcc77795d717326c53
Leandro Velasco <pino2110@gmail.com>
2020-08-18 12:14:28 -0500

    Feature: Recipe View

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import MainView from '../views/MainView/MainView.vue'
+import RecipeView from '../views/RecipeView/RecipeView.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'MainView',
     component: MainView
+  },
+  {
+    path: '/recipeview',
+    name: 'RecipeView',
+    component: RecipeView
   }
 ]
 
