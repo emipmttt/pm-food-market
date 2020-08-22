9ef517ae7f5388e60987aff7c9fc4e844896852c
Mario Silva <mario.silvae@outlook.com>
2020-08-22 12:31:23 -0500

    Feature footer component

---
+++ src/components/Footer/Footer.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="footer">
+    <span>Platzi Master Project</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+
+<style lang="scss" scoped>
+.footer {
+  height: 50px;
+  width: 100%;
+  margin-top: 40px;
+  display: flex;
+  background-color: #f4a261;
+  & span {
+    color: white;
+    padding-left: 10px;
+    justify-content: start;
+    align-self: center;
+  }
+}
+</style>
