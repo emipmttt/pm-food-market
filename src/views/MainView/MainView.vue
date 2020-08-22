d704f6a402b526305ddf8b85083011f325666d7c
Mario Silva <mario.silvae@outlook.com>
2020-08-22 12:24:53 -0500

    Update MainView with components

--- src/views/MainView/MainView.vue
+++ src/views/MainView/MainView.vue
@@ -1,10 +1,46 @@
 <template>
-  <div></div>
+  <div>
+    <Header />
+    <section>
+      <ToggleFilter />
+      <ToggleFilter />
+      <ToggleFilter />
+      <ToggleFilter />
+      <ToggleFilter />
+      <ToggleFilter />
+      <ToggleFilter />
+      <ToggleFilter />
+    </section>
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Footer />
+  </div>
 </template>
 
 <script>
-export default {};
+import Header from "../../components/Header/Header";
+import Card from "../../components/Card/Card";
+import ToggleFilter from "../../components/ToggleFilter/ToggleFilter";
+import Footer from "../../components/Footer/Footer";
+
+export default {
+  components: {
+    Header,
+    Card,
+    ToggleFilter,
+    Footer,
+  },
+};
 </script>
 
 <style>
+body {
+  margin: 0;
+}
 </style>
