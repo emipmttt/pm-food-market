9221bd8fe6b776e34f5f1612996a21f9e49e664f
Emiliano Pamont <emipmttt@gmail.com>
2020-09-02 11:30:23 -0500

    update/upload JSON in recipes

--- src/views/Recipes/Recipes.vue
+++ src/views/Recipes/Recipes.vue
@@ -1,91 +1,130 @@
 <template>
-  <main
-    :style="`background-image:url('${require('@/assets/img/path3622.svg')}');background-repeat: no-repeat; background-size: 60% auto`"
-  >
-    <section class="main-container">
-      <h1 class="h1">Recipes</h1>
-      <table class="recipe-table">
-        <tbody>
-          <tr>
-            <td>cooking recipe</td>
-            <td>
-              <router-link to="/manage-recipe">
-                <span class="material-icons">edit</span>
-              </router-link>
-            </td>
-          </tr>
-          <tr>
-            <td>cooking recipe</td>
-            <td>
-              <router-link to="/manage-recipe">
-                <span class="material-icons">edit</span>
-              </router-link>
-            </td>
-          </tr>
-          <tr>
-            <td>cooking recipe</td>
-            <td>
-              <router-link to="/manage-recipe">
-                <span class="material-icons">edit</span>
-              </router-link>
-            </td>
-          </tr>
-          <tr>
-            <td>cooking recipe</td>
-            <td>
-              <router-link to="/manage-recipe">
-                <span class="material-icons">edit</span>
-              </router-link>
-            </td>
-          </tr>
-          <tr>
-            <td>cooking recipe</td>
-            <td>
-              <router-link to="/manage-recipe">
-                <span class="material-icons">edit</span>
-              </router-link>
-            </td>
-          </tr>
-          <tr>
-            <td>cooking recipe</td>
-            <td>
-              <router-link to="/manage-recipe">
-                <span class="material-icons">edit</span>
-              </router-link>
-            </td>
-          </tr>
-          <tr>
-            <td>cooking recipe</td>
-            <td>
-              <router-link to="/manage-recipe">
-                <span class="material-icons">edit</span>
-              </router-link>
-            </td>
-          </tr>
-          <tr>
-            <td>cooking recipe</td>
-            <td>
-              <router-link to="/manage-recipe">
-                <span class="material-icons">edit</span>
-              </router-link>
-            </td>
-          </tr>
-          <tr>
-            <td>cooking recipe</td>
-            <td>
-              <router-link to="/manage-recipe">
-                <span class="material-icons">edit</span>
-              </router-link>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </section>
-  </main>
+  <div>
+    <AdminSideNav />
+    <main
+      :style="`background-image:url('${require('@/assets/img/path3622.svg')}');background-repeat: no-repeat; background-size: 60% auto`"
+    >
+      <section class="main-container">
+        <h1 class="h1">Recipes</h1>
+
+        <form @submit.prevent="upload_json">
+          <textarea v-model="json_data" placeholder="JSON Data"></textarea>
+          <br />
+          <button>Add Data</button>
+        </form>
+
+        <table class="recipe-table">
+          <tbody>
+            <tr>
+              <td>cooking recipe</td>
+              <td>
+                <router-link to="/manage-recipe">
+                  <span class="material-icons">edit</span>
+                </router-link>
+              </td>
+            </tr>
+            <tr>
+              <td>cooking recipe</td>
+              <td>
+                <router-link to="/manage-recipe">
+                  <span class="material-icons">edit</span>
+                </router-link>
+              </td>
+            </tr>
+            <tr>
+              <td>cooking recipe</td>
+              <td>
+                <router-link to="/manage-recipe">
+                  <span class="material-icons">edit</span>
+                </router-link>
+              </td>
+            </tr>
+            <tr>
+              <td>cooking recipe</td>
+              <td>
+                <router-link to="/manage-recipe">
+                  <span class="material-icons">edit</span>
+                </router-link>
+              </td>
+            </tr>
+            <tr>
+              <td>cooking recipe</td>
+              <td>
+                <router-link to="/manage-recipe">
+                  <span class="material-icons">edit</span>
+                </router-link>
+              </td>
+            </tr>
+            <tr>
+              <td>cooking recipe</td>
+              <td>
+                <router-link to="/manage-recipe">
+                  <span class="material-icons">edit</span>
+                </router-link>
+              </td>
+            </tr>
+            <tr>
+              <td>cooking recipe</td>
+              <td>
+                <router-link to="/manage-recipe">
+                  <span class="material-icons">edit</span>
+                </router-link>
+              </td>
+            </tr>
+            <tr>
+              <td>cooking recipe</td>
+              <td>
+                <router-link to="/manage-recipe">
+                  <span class="material-icons">edit</span>
+                </router-link>
+              </td>
+            </tr>
+            <tr>
+              <td>cooking recipe</td>
+              <td>
+                <router-link to="/manage-recipe">
+                  <span class="material-icons">edit</span>
+                </router-link>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </section>
+    </main>
+  </div>
 </template>
 
 <script>
-export default {};
+import firebase from "@/api/firebase";
+import AdminSideNav from "@/components/AdminSideNav/AdminSideNav";
+export default {
+  data() {
+    return {
+      json_data: "",
+    };
+  },
+  components: {
+    AdminSideNav,
+  },
+  methods: {
+    upload_json() {
+      const data = JSON.parse(this.json_data);
+
+      var db = firebase.firestore();
+
+      var batch = db.batch();
+
+      data.forEach((recipe) => {
+        var recipeRef = db.collection("recipes").doc();
+        batch.set(recipeRef, recipe);
+      });
+
+      batch.commit().then(function () {
+        alert("Recetas actualizadas correctamente");
+      });
+    },
+  },
+};
 </script>
 
 <style lang="scss">
