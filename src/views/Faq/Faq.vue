6a24dbfca2317485fa2aa8c32be82c27f3e62688
Johan Galeano <johans.galeanot@hotmail.com>
2020-09-23 16:53:50 -0500

    fix/ faq sin estilos

--- src/views/Faq/Faq.vue
+++ src/views/Faq/Faq.vue
@@ -1,17 +1,44 @@
 <template>
 <div>
   <Header></Header>
-  Hola
+  <section class="main-container">
+    <h1 class="h1">Frequent questions</h1>
+    <collapsible :items="collapsibleItem"></collapsible>
+  </section>
 </div>
 </template>
 
 <script>
 import Header from "../../components/Header/Header";
+import Collapsible from "../../components/Collapsible/Collapsible";
 
 export default {
   name: 'faq',
+  data(){
+    return {
+      collapsibleItem: [
+        {
+          header: 'Header 1',
+          body:'Lorem, ipsum dolor sit amet consectetur adipisicing elit.'
+        },
+        {
+          header: 'Header 2',
+          body:'Lorem, ipsum dolor sit amet consectetur adipisicing elit.'
+        },
+        {
+          header: 'Header 3',
+          body:'Lorem, ipsum dolor sit amet consectetur adipisicing elit.'
+        },
+        {
+          header: 'Header 4',
+          body:'Lorem, ipsum dolor sit amet consectetur adipisicing elit.'
+        },
+      ]
+    }
+  },
   components: {
     Header,
+    Collapsible
   },
 
 };
