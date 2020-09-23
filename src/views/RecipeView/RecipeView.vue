9282236e3d694751ab37c564300a4c66e77b92ca
Leandro Velasco <pino2110@gmail.com>
2020-09-22 23:09:54 -0500

    Fix - recipeView position

--- src/views/RecipeView/RecipeView.vue
+++ src/views/RecipeView/RecipeView.vue
@@ -213,11 +213,9 @@ h2 {
 }
 
 article {
-  display: grid;
   margin: 0 auto;
   width: 100%;
-
-  height: 650px;
+  height: 610px;
 
   p {
     font-style: normal;
@@ -265,7 +263,6 @@ article {
 
 .recipeView__contenido {
   display: inline-block;
-  padding-left: 20px;
   position: relative;
   -webkit-box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14),
     0 3px 1px -2px rgba(0, 0, 0, 0.12), 0 1px 5px 0 rgba(0, 0, 0, 0.2);
